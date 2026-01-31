372547bbe86f6dcd2d81f3449369d82b0ff5672a
gobbyo <jbeman@hotmail.com>
2026-01-30 19:09:34 -0800

    updates to hall effect program

--- halleffect/hall_effect_simple_test_with_light.py
+++ halleffect/hall_effect_simple_test_with_light.py
@@ -1,9 +1,15 @@
 from machine import Pin
+import neopixel
 import time
+
+MAX_BRIGHTNESS = 127  # Set to a value between 0 and 255
 
 # For digital hall effect sensor (detects presence/absence)
 def read_sensor():
     hall_sensor = Pin(26, Pin.IN, Pin.PULL_UP)
+    light = Pin(23, Pin.OUT)   # change to your pin
+    np = neopixel.NeoPixel(light, 1)
+
     last_state = None  # Track previous state
     
     while True:
@@ -13,8 +19,12 @@ def read_sensor():
         if current_state != last_state:
             if current_state == 0:
                 print("South pole detected")
+                np[0] = (0, MAX_BRIGHTNESS, 0)  # Green
+                np.write()
             else:
                 print("No magnet")
+                np[0] = (0, 0, 0)
+                np.write()
             last_state = current_state
         
         time.sleep(0.005)  # Check more frequently for state changes
